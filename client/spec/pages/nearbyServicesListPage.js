2de6deaafa9efc015a13b99d265701e1aa52dc0d
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-09-20 20:50:11 +1000

    Laura/Sheena: added nearbyServicesListPage spec & actual page

---
+++ client/spec/pages/nearbyServicesListPage.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var NearbyServicesList = require('../../src/js/pages/nearbyServicesListPage'),
+  App = require('../../src/js/app'),
+  eventHub = require('watch_framework').EventHub,
+  page;
+
+describe('The Nearby Health Services List', function() {
+
+  beforeEach(function() {
+    page = new NearbyServicesList();
+  });
+
+  describe('button events', function() {
+
+    describe('top', function() {
+      it('should move the selection to the previous nearby service', function() {
+        spyOn(page, 'previous');
+        page.configureButtons();
+        eventHub.trigger('top');
+        expect(page.previous).toHaveBeenCalled();
+      });
+    });
+
+    describe('right', function() {
+      it('should select the current service and navigate to the more detailed info about service', function() {
+        spyOn(page, 'select');
+        page.configureButtons();
+        eventHub.trigger('right');
+        expect(page.select).toHaveBeenCalled();
+      });
+    });
+
+    describe('bottom', function() {
+      it('should move the selection to the next service', function() {
+        spyOn(page, 'next');
+        page.configureButtons();
+        eventHub.trigger('bottom');
+        expect(page.next).toHaveBeenCalled();
+      });
+    });
+
+    describe('left', function() {
+      it('should go back to previous basic list of services', function() {
+        spyOn(page, 'back');
+        page.configureButtons();
+        eventHub.trigger('left');
+        expect(page.back).toHaveBeenCalled();
+      });
+    });
+
+  });
+
+  describe('select', function() {
+    it('navigates to the appHome page', function() {
+      var cid = 'c42';
+      spyOn(window.App, 'navigate');
+      page.selected = { cid: cid };
+      page.select();
+      expect(window.App.navigate).toHaveBeenCalledWith('appDirectory/' + cid);
+    });
+  });
+
+});
---
+++ client/src/js/pages/nearbyServicesListPage.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Menu = require('watch_framework').Menu,
+  storage = require('../../storage');
+
+// NOTE you can use a custom template like any other backbone page
+// NOTE you can load JSON files by modifying src/storage/index.js
+
+var nearbyServicesList = Menu.extend({
+
+  id: 'nearby-menu',
+
+  buttonEvents: {
+    //right: 'select',
+    //left: 'back',
+    //top: 'previous',
+    //bottom: 'next'
+  },
+
+  getMenuItemLabel: function(menuItem) {
+    return menuItem.get('name');
+  },
+
+  select: function() {
+    // TODO make the router a singleton?
+    // TODO move showPage in to the router, might be able to get rid of the activePage stuff too
+    // TODO include it at the top level instead of inside the app?
+
+    //window.App.navigate('appDirectory/' + this.selected.cid);
+  },
+
+  collection: storage.eventsData
+
+});
+
+module.exports = nearbyServicesList;
