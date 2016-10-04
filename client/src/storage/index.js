0bab3497475251cc9b04521cf05391c365a374b1
xchrisle <chrisle1994@gmail.com>
2016-10-04 20:28:20 +1100

    Chris/Pauline: add dummy data in json file

--- client/src/storage/index.js
+++ client/src/storage/index.js
@@ -1,6 +1,17 @@
+// 'use strict';
+//
+// var eventsData = require("json!./events.json");
+//
+// function Storage() {
+//   this.eventsData = new Backbone.Collection();
+//   this.eventsData.add(eventsData.events);
+// }
+//
+// module.exports = new Storage();
+
 'use strict';
 
-var eventsData = require("json!./events.json");
+var eventsData = require("json!./categories.json");
 
 function Storage() {
   this.eventsData = new Backbone.Collection();
