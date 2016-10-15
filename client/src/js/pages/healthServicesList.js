631e1b7e6decd1efc98408351da7d461a858ce2a
Pauline Kelly <pauline.m.kelly1@gmail.com>
2016-10-15 13:48:31 +1100

    Sheena,Pauline: writes data into template for health service list

--- client/src/js/pages/healthServicesList.js
+++ client/src/js/pages/healthServicesList.js
@@ -8,12 +8,6 @@ var appServices = Page.extend({
 
   id: 'healthServicesList',
 
-  // services: ['Dentist',
-  //           'Doctor',
-  //           'Counsellor',
-  //           'Hospital',
-  //           'Pharmacist'],
-
   data: require('../../storage').categoriesData,
 
   template: require('../../templates/pages/healthServicesList.hbs'),
@@ -32,11 +26,6 @@ var appServices = Page.extend({
     window.App.navigate('appDirectory');
   },
 
-  /**
-  Using this function should find the selected item, and navigate towards the page for that information.
-
-  Would be great if it could use the info associated with the json object.
-  */
   goToHealthList: function() {
     window.App.navigate('healthList');
   },
@@ -46,32 +35,10 @@ var appServices = Page.extend({
   },
 
   render: function() {
-
-    // var models = ourData['models'];
-    // console.log('This is ourData:', ourData);
-    // console.log('These are our models:', ourData.models);
-    //
-    // console.log('This is model 0:', ourData.models[0]['attributes']);
-    // console.log('This is model attribute:', ourData['models'][0]['attributes']['category_name']);
-
-    this.$el.html(this.template(ourData));
+    var healthOptions = ourData.models[0].attributes.options;
+    this.$el.html(this.template({healthOptions: healthOptions}));
     return this;
   },
-
-  // getServiceTypeData: function() {
-  //   return this.data.get(this.options.cid);
-  // },
-  //
-  // render: function() {
-  //   // TODO if showPage was creataing a new instance of this view the model could be passed in
-  //   var details = this.getServiceTypeData().toJSON();
-  //   this.$el.html(this.template(details));
-  //   return this;
-  // }
-
-  // scrollUp: function() {
-  //   $('#watch-face').animate({scrollTop: '-=70px'});
-  // },
 
   scrollUp: function() {
     $('#watch-face').animate({scrollTop: '-=35px'});
